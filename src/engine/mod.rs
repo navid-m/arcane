15feb931d3d7e52ece58b4be636e9113bee67a1a
Navid M <navidmomtahen@gmail.com>
2026-02-27 15:15:17 +0000

    add comments

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,3 +1,9 @@
+//! The storage engine of ArcaneDB.
+//!
+//! This module provides the core functionality for managing and interacting with the storage engine.
+//!
+//! It includes features such as bucket creation, data insertion, deletion, and update operations.
+
 use crate::error::{ArcaneError, Result};
 use crate::parser::{self, Filter, Projection, Statement};
 use crate::storage::{BucketStore, FieldDef, Record, Schema, Value};
--- src/meta/mod.rs
+++ src/meta/mod.rs
@@ -1,3 +1,7 @@
+//! Meta version information module.
+//!
+//! Provides functionality to retrieve and display the version of ArcaneDB.
+
 /// Show the version of Arcane.
 pub fn show_version() {
     println!("ArcaneDB v{} - Copyright (C) Navid M 2026", get_version());
